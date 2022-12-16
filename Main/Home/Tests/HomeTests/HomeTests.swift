517941e31aef3d37f4538a7cbcc3646ebff89994
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-12-16 19:35:52 +0900

    add test

--- Main/Home/Tests/HomeTests/HomeTests.swift
+++ Main/Home/Tests/HomeTests/HomeTests.swift
@@ -1,10 +1,59 @@
 import XCTest
 import Quick
 import Nimble
+import ComposableArchitecture
 @testable import Home
 
-final class HomeTests: XCTestCase {
-    func testExample() throws {
-       
+final class HomeTests: QuickSpec {
+    
+    override func spec() {
+        let store = TestStore(initialState: HomeFeature.State(),
+                            reducer: HomeFeature())
+        describe("画面表示時") {
+            context("PuttingViewを開く場合") {
+                store.send(.togglePuttiingView) {
+                    $0.isPuttingView.toggle()
+                    expect(store.state.isSettingView).to(equal(true))
+                }
+            }
+            
+            context("おやつリスト画面を開く場合") {
+                store.send(.toggleSweetListView) {
+                    $0.isSweetListView.toggle()
+                    expect(store.state.isSweetListView).to(equal(true))
+                }
+            }
+            
+            context("設定画面を開く場合") {
+                store.send(.toggleSettingView) {
+                    $0.isSettingView.toggle()
+                    expect(store.state.isSettingView).to(equal(true))
+                }
+            }
+        }
+        
+        
+        describe("画面を閉じる時") {
+            context("PuttingViewを閉じる場合") {
+                store.send(.togglePuttiingView) {
+                    $0.isPuttingView.toggle()
+                    expect(store.state.isSettingView).to(equal(false))
+                }
+            }
+
+            context("おやつリスト画面を閉じる場合") {
+                store.send(.toggleSweetListView) {
+                    $0.isSweetListView.toggle()
+                    expect(store.state.isSweetListView).to(equal(false))
+                }
+            }
+
+            context("設定画面を閉じる場合") {
+                store.send(.toggleSettingView) {
+                    $0.isSettingView.toggle()
+                    expect(store.state.isSettingView).to(equal(false))
+                }
+            }
+        }
     }
 }
