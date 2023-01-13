e026c484adcaa5c8f202b9a4820f6651b91404cf
Katopan <76249494+katomizuki@users.noreply.github.com>
2023-01-13 20:06:51 +0900

    add feature

--- Main/Home/Sources/SweetDetailFeature/PropertiesSweetView.swift
+++ Main/Home/Sources/SweetDetailFeature/PropertiesSweetView.swift
@@ -13,67 +13,67 @@ struct PropertiesSweetView: View {
     private let store: StoreOf<SweetDetailFeature>
     
     var body: some View {
-        VStack {
-            VStack(alignment: .leading,
-                   spacing: 12) {
-                Text("flower")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                
-                Text("aaaaaaaaaaa")
-                    .fontWeight(.semibold)
-                    .foregroundColor(.gray)
-                    
-                Label {
-                    Text("sss")
-                        .fontWeight(.semibold)
-                } icon: {
-                    Image(systemName: "star.fill")
-                }
-                .foregroundColor(Color("Gold"))
-
-            }
-            .padding(.top, 30)
-            .frame(maxWidth: .infinity,
-                   alignment: .leading)
-            
-            
-            HStack(alignment: .top) {
-                Button {
-                    
-                } label: {
-                    VStack(spacing: 12) {
-                        Image(systemName: "plus.square.dashed")
-                            .resizable()
-                            .renderingMode(.template)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 45,
-                                   height: 45)
-                        
-                        Text("Decide!!")
-                            .fontWeight(.semibold)
-                            .padding(.top, 20)
-                    }
-                    .foregroundColor(.black)
-                    .padding(18)
-                    .background {
-                        RoundedRectangle(cornerRadius: 20,
-                                         style: .continuous)
-                            .fill(.white)
-                    }
-                }
+        WithViewStore(self.store) { viewStore in
+            VStack {
                 VStack(alignment: .leading,
-                       spacing: 10) {
-                    Text("説明が入るよ/n説明が入るよ/n説明が入るよ/n説明が入るよ/n説明が入るよ/n説明が入るよ/n説明が入るよ")
-                        .font(.callout)
+                       spacing: 12) {
+                    Text(viewStore.state.sweet.name)
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                    
+                    Text(viewStore.state.sweet.name)
                         .fontWeight(.semibold)
                         .foregroundColor(.gray)
+                        
+                    Label {
+                        Text(viewStore.state.sweet.name)
+                            .fontWeight(.semibold)
+                    } icon: {
+                        Image(systemName: "star.fill")
+                    }
+                    .foregroundColor(Color.orange)
 
                 }
+                .padding(.top, 30)
                 .frame(maxWidth: .infinity,
                        alignment: .leading)
+                HStack(alignment: .top) {
+                    Button {
+                        
+                    } label: {
+                        VStack(spacing: 12) {
+                            Image(systemName: "plus.square.dashed")
+                                .resizable()
+                                .renderingMode(.template)
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 45,
+                                       height: 45)
+                            
+                            Text("Decide!!")
+                                .fontWeight(.semibold)
+                                .padding(.top, 20)
+                        }
+                        .foregroundColor(.black)
+                        .padding(18)
+                        .background {
+                            RoundedRectangle(cornerRadius: 20,
+                                             style: .continuous)
+                                .fill(.white)
+                        }
+                    }
+                    VStack(alignment: .leading,
+                           spacing: 10) {
+                        Text(viewStore.state.sweet.description)
+                            .font(.callout)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.gray)
+
+                    }
+                    .frame(maxWidth: .infinity,
+                           alignment: .leading)
+                }
+                .padding(.top, 30)
             }
-            .padding(.top, 30)
         }
     }
 
